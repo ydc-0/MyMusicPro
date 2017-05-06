91791dc0dd41ac2c1bb3d2b414816fb6a79dccb2
yudongchen95 <xd_ydchen@163.com>
2017-05-06 23:09:08 +0800

    main ui

--- app/src/main/java/cn/edu/xidian/www/mymusicpro/MainActivity.java
+++ app/src/main/java/cn/edu/xidian/www/mymusicpro/MainActivity.java
@@ -1,13 +1,44 @@
 package cn.edu.xidian.www.mymusicpro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import cn.edu.xidian.www.mymusicpro.music.MusicList;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "MainActivity";
+    public MusicList main_music_liat = new MusicList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.main_title);
+
+        main_music_liat.InitMusicList();
+
+
+    }
+
+    public void StartLocalMusic(View view) {
+        Log.i(TAG, "StartLocalMusic: start.");
+        Intent intent = new Intent(this, MusicListActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);    }
+
+    public void MusicRecommendation(View view) {
+        Log.i(TAG, "MusicRecommendation: start.");
+
+    }
+
+    public void EnterSettings(View view) {
+        Log.i(TAG, "EnterSettings: start.");
+
     }
 }
---
+++ app/src/main/java/cn/edu/xidian/www/mymusicpro/music/MusicList.java
@@ -0,0 +1,12 @@
+package cn.edu.xidian.www.mymusicpro.music;
+
+/**
+ * Created by chenyudong on 2017/5/6.
+ */
+
+public class MusicList {
+
+    public void InitMusicList(){
+
+    }
+}
